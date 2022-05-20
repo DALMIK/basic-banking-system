3871590434f4ac4fe5b4cf178ff2fda5e0aaa714
DALMIK <89015969+DALMIK@users.noreply.github.com>
2022-05-20 18:19:15 +0530

    Add files via upload

---
+++ BBS/index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+  <title>Basic Banking System</title>
+</head>
+
+<body>
+  <?php
+  include('navbar.php');
+  ?>
+
+  <div class="container-fluid">
+    <!-- Introduction section -->
+    <div class="row intro py-1">
+      <div class="col-sm-12 col-md">
+        <div class="heading text-center my-0">
+          <p id="aaa">BASIC</p>
+          <p id="aaaa">BANKING SYSTEM</p>
+          <p id="aaaaa"><i>Transfer Your money In a Easiest way.</i> </p>
+        </div>
+      </div>
+    </div>
+
+    <footer class="text-center mt-5 py-2">
+      <p>The Sparks Foundation &copy 2022 </p>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ BBS/navbar.php
@@ -0,0 +1,23 @@
+<!-- navbar -->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php"><b>BASIC BANKING SYSTEM</b></a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav me-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php" padding-left:20px;"><b>HOME</b></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="createuser.php" padding-left:20px;"><b>CREATE USER</b></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="transfermoney.php" padding-left:20px;"><b>TRANSFER MONEY</b></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="transactionhistory.php" padding-left:20px;"><b>TRANSACTION HISTORY</b></a>
+      </li>
+  </div>
+</nav>
